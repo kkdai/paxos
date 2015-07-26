2b22a2e37e733a23c7f96180acc4fa237a77ebcc
Evan Lin <evanslin@gmail.com>
2015-07-26 17:37:56 +0800

    Remove init chan to make it wait until time out

--- network.go
+++ network.go
@@ -1,12 +1,15 @@
 package paxos
 
-import "log"
+import (
+	"log"
+	"time"
+)
 
 func CreateNetwork(nodes ...int) *network {
 	nt := network{recvQueue: make(map[int]chan message, 0)}
 
 	for _, node := range nodes {
-		nt.recvQueue[node] = make(chan message, 1024)
+		nt.recvQueue[node] = make(chan message)
 	}
 
 	return &nt
@@ -16,15 +19,21 @@ type network struct {
 	recvQueue map[int]chan message
 }
 
-func (n *network) sendTo(from int, m message) {
+func (n *network) sendTo(m message) {
 	log.Println("Send msg from:", m.from, " send to", m.to, " val:", m.val)
 	n.recvQueue[m.to] <- m
 }
 
 func (n *network) recevFrom(id int) message {
-	retMsg := <-n.recvQueue[id]
-	log.Println("Recev msg from:", retMsg.from, " send to", retMsg.to, " val:", retMsg.val)
-	return retMsg
+	select {
+	case retMsg := <-n.recvQueue[id]:
+		log.Println("Recev msg from:", retMsg.from, " send to", retMsg.to, " val:", retMsg.val)
+		return retMsg
+	case <-time.After(time.Second):
+		log.Println("id:", id, " don't get message.. time out.")
+		return message{}
+	}
+
 }
 
 type nodeNetwork struct {
@@ -33,7 +42,7 @@ type nodeNetwork struct {
 }
 
 func (n *nodeNetwork) send(m message) {
-	n.net.sendTo(m.to, m)
+	n.net.sendTo(m)
 }
 
 func (n *nodeNetwork) recev() message {
--- paxos_test.go
+++ paxos_test.go
@@ -5,16 +5,17 @@ import "testing"
 func TestBasicNetwork(t *testing.T) {
 	nt := CreateNetwork(1, 3, 5, 2, 4)
 	go func() {
+		nt.recevFrom(5)
 		nt.recevFrom(1)
 		nt.recevFrom(3)
 		nt.recevFrom(2)
-
+		nt.recevFrom(4)
 	}()
 
-	m3 := message{from: 4, to: 2, typ: Promise, seq: 3, preSeq: 2, val: "m3"}
-	nt.sendTo(4, m3)
 	m1 := message{from: 3, to: 1, typ: Prepare, seq: 1, preSeq: 0, val: "m1"}
-	nt.sendTo(3, m1)
+	nt.sendTo(m1)
 	m2 := message{from: 5, to: 3, typ: Accept, seq: 2, preSeq: 1, val: "m2"}
-	nt.sendTo(5, m2)
+	nt.sendTo(m2)
+	m3 := message{from: 4, to: 2, typ: Promise, seq: 3, preSeq: 2, val: "m3"}
+	nt.sendTo(m3)
 }
