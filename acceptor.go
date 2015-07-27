411c43beff963787a06ea4e49914d9f69898dad4
Evan Lin <evanslin@gmail.com>
2015-07-28 01:31:44 +0800

    starting learner

--- acceptor.go
+++ acceptor.go
@@ -16,6 +16,32 @@ type acceptor struct {
 	nt         nodeNetwork
 }
 
+//Acceptor process detail logic.
+func (a *accept) run() {
+	for {
+		m := a.nt.receive()
+		switch m.typ {
+		case Prepare:
+			promiseMsg := a.recevPrepare(m)
+			a.nt.send(promiseMsg)
+			continue
+		case Propose:
+			accepted := a.recevPropose(m)
+			if accepted {
+
+				for _, learner := range learners {
+					m.aafrom = a.id
+					m.to = learner
+					m.typ = Accept
+					a.nt.send(m)
+				}
+			}
+		default:
+			log.Fatalln("Unsupport message in accpetor ID:", a.id)
+		}
+	}
+}
+
 //After acceptor receive prepare message.
 //It will check  prepare number and return acceptor if it is bigest one.
 func (a *acceptor) recevPrepare(prepare message) *message {
@@ -34,13 +60,11 @@ func (a *acceptor) recevPrepare(prepare message) *message {
 
 //Recev Propose only check if acceptor already accept bigger propose before.
 //Otherwise, will just forward this message out and change its type to "Accept" to learning later.
-func (a *acceptor) recevPropose(proposeMsg message) *message {
+func (a *acceptor) recevPropose(proposeMsg message) bool {
 	//Already accept bigger propose before
-	if a.acceptMsg.getProposeSeq() > proposeMsg.getProposeSeq() {
+	if a.acceptMsg.getProposeSeq() > proposeMsg.getProposeSeq() || a.acceptMsg.getProposeSeq() < promiseMsg.getProposeSeq() {
 		log.Println("ID:", a.id, " acceptor not take propose:", proposeMsg.val)
-		return nil
+		return false
 	}
-	a.acceptMsg = proposeMsg
-	proposeMsg.typ = Accept
-	return &proposeMsg
+	return true
 }
---
+++ learner.go
@@ -0,0 +1,4 @@
+package paxos
+
+type learner struct {
+}
